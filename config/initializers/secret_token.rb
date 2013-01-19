7adfc4ee95e801be94451f8d1e9d486b64311631
Artur <artkopacz@gmail.com>
2013-01-19 12:33:22 +0100

    Sam Devise dziala.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TmpDeviseAndNestedModelForms::Application.config.secret_token = '108e9cc837edb869f873eb77ba97628d04b3e498b36a457f339ab74e8c404554ca7f83e832ce631270fce02a322e1cfd99f1580b77b39486a9707d294bc03996'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TmpDeviseAndNestedModelForms::Application.config.session_store :cookie_store, key: '_tmp--devise_and_nested_model_forms_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TmpDeviseAndNestedModelForms::Application.config.session_store :active_record_store
